ffa160987b71ec0b404588244c435961d234b951
mohemohe <mohemohe@ghippos.net>
2022-08-21 15:27:35 +0900

    faviconとか追加

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { execSync } from "child_process";
+import path from "path";
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { VitePWA } from "vite-plugin-pwa";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 const fullHash = execSync("git rev-parse HEAD").toString().trim();
-const shortHash = execSync("git rev-parse --short HEAD").toString().trim();
 
 export default defineConfig(({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, __dirname), VITE_GIT_HASH: fullHash };
@@ -20,7 +22,6 @@ export default defineConfig(({ mode }) => {
     build: {
       sourcemap: process.env.AWS_CDK !== "1",
       outDir: process.env.AWS_CDK === "1" ? "/asset-output/" : "./dist/",
-      assetsDir: `assets.${shortHash}`,
     },
     plugins: [
       tsconfigPaths({
@@ -33,6 +34,40 @@ export default defineConfig(({ mode }) => {
             plugins: ["decorators-legacy", "classProperties", "@emotion/babel-plugin"],
           },
         },
+      }),
+      VitePWA({
+        manifest: {
+          name: "RemoWeb",
+          short_name: "RemoWeb",
+          description: "Web client for Nature Remo",
+          background_color: "#000000",
+          theme_color: "#90caf9",
+          icons: [
+            {
+              src: "/assets/pwa-192x192.png",
+              sizes: "192x192",
+              type: "image/png",
+            },
+            {
+              src: "/assets/pwa-512x512.png",
+              sizes: "512x512",
+              type: "image/png",
+            },
+          ],
+        },
+        injectRegister: null,
+      }),
+      viteStaticCopy({
+        targets: [
+          {
+            src: path.resolve(__dirname, "src/assets/pwa-192x192.png"),
+            dest: "assets",
+          },
+          {
+            src: path.resolve(__dirname, "src/assets/pwa-512x512.png"),
+            dest: "assets",
+          },
+        ],
       }),
     ],
     esbuild: {
